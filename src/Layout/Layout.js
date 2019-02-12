f8c04a9777ea44be446210d402dee8d32e23e8b6
Igor Sergiienko <isergiienko@evolutiongaming.com>
2019-02-12 17:00:02 +0200

    Use styled-components in layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import styles from './Layout.module.css';
+import styled from 'styled-components'
+
+const border = 'border: 2px dashed #1890ff'
+
+const Wrapper = styled.div`
+  max-width: 820px;
+  margin: 32px auto;
+  padding: 16px;
+  background-color: #fff;
+`;
+
+const Header = styled.header`
+  line-height: 2em;
+  font-size: 2em;
+  text-align: center;
+  margin-bottom: 16px;
+  ${border}
+`;
+
+const Main = styled.main`
+  min-height: 320px;
+  padding: 16px;
+  text-align: center;
+  ${border}
+`;
+
 
 export const Layout = ({ children }) => (
-  <div className={styles.app}>
-    <header className={styles.header}>Crew applications app</header>
-    <main className={styles.main}>
-      {children}
-    </main>
-  </div>
+  <Wrapper>
+    <Header>Crew applications app</Header>
+    <Main>{children}</Main>
+  </Wrapper>
 );
